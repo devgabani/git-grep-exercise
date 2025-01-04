23a490b2aacebebc191b6dbb643b917854a17480
Dev Gabani <gabanid49@gmail.com>
2025-01-04 13:48:29 +0530

    Add script.py with TODO comment

--- script.py
+++ script.py
@@ -1,2 +1,1 @@
-def hello_world():\n   # TODO: Implement this function\n pass
-def new_function():\n  #  FIXME: This needs refactoring\n pass
+def hello_world():\n  # TODO: Implement this function\n pass
